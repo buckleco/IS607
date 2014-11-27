36a4e2659face0dd7fd4ce0c8047cc5c18f192ee
buckleco <conorbuckley@mail.com>
2014-11-26 23:30:39 -0500

    Project 5

---
+++ 607_project5_week13.sql
@@ -0,0 +1,8 @@
+﻿--find the working directory - used to ensure I placed the csv file in the correct directory
+--SHOW data_directory;
+
+COPY (SELECT * 
+FROM chiparks
+INNER JOIN parkclass ON parkclass."parkclass_number" = chiparks."parkclass_number"
+INNER JOIN wards ON wards."ward_number" = chiparks."ward_number"
+INNER JOIN zips ON zips."zip_number" = chiparks."zip_number") TO  'C:/Program Files/PostgreSQL/9.3/data/pg_output13.csv' WITH CSV header;
